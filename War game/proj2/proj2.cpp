49b6854a88ec6ec88432bd9390777dad217bf78d
Stevecab97 <79380779+Stevecab97@users.noreply.github.com>
2025-11-19 17:58:15 -0500

    Added War Game file

---
+++ War game/proj2/proj2.cpp
@@ -0,0 +1,14 @@
+#include "Troop.h"
+#include "Game.h"
+
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main () {
+  Game newGame;
+
+  return 0;
+}
